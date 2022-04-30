5619cdd070561a4de6269c49175db319dc8176b5
webdeveloperisaac <isaacinfrastructure@gmail.com>
2022-04-30 11:07:41 +0530

    intitial with a counter hook for setstate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
-function App() {
+const App = () => {
+
+const [counter, setCounter] = useState(1);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={()=>setCounter((prevCount)=>prevCount+1)}>+</button>
+      <h1>{counter}</h1>
+      <button onClick={()=>setCounter((prevCount)=>(prevCount)-1)}>-</button>
+      
     </div>
   );
 }
