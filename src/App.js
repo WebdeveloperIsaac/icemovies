4919825f940a785b3b2f7a7f937897e19afe13c0
webdeveloperisaac <isaacinfrastructure@gmail.com>
2022-05-02 17:53:37 +0530

    Use State hook with props implemented for Counter Functioncality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import { useState } from "react";
 import './App.css';
 
+const Leaders = (props) => {
+  return(
+    <>
+      <h1>Name : {props.name}</h1>
+      <h2>Age : 20</h2>
+   </>
+  );
+}
+
 const App = () => {
 
 const [counter, setCounter] = useState(1);
-
+const name ={name:"isaac",age:20};
+const [timer,setTimer]= useState(1)
   return (
     <div className="App">
       <button onClick={()=>setCounter((prevCount)=>prevCount+1)}>+</button>
       <h1>{counter}</h1>
       <button onClick={()=>setCounter((prevCount)=>(prevCount)-1)}>-</button>
-      
+      <button onClick={()=>setTimer((prevCount)=>(prevCount)+1)}>{timer}
+      </button>
+      <h1>Hai {name.name}</h1>
+      <Leaders name={name.name}/>
     </div>
   );
 }
