a4193bd0e0e9b0c10593850be453d2023389021d
webdeveloperisaac <isaacinfrastructure@gmail.com>
2022-05-04 17:24:11 +0530

    Added the loadfor the component from Search.title

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import { useState, useEffect } from "react";
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import MovieCard from "./MovieCard";
+import SearchIcon from "./search.svg";
+import "./App.css";
+
+const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
 
-const [counter, setCounter] = useState(1);
- 
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
 
-//the useEffect Hook actually is a function returning a Function and have to pass the second parameter empty array to work fine
-// and also we have the setCounter not just change the state directly with code
-useEffect(()=>{
-  alert('You changed the COunter to')
-},[counter]);
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
 
-return (
-    <div className="App">
-      <button onClick={()=>setCounter((prevCount)=>prevCount+1)}>+</button>
-      <h1 onClick={() => alert("The Counter is Working Fine")}>{counter}</h1>
-      <button onClick={()=>setCounter((prevCount)=>(prevCount)-1)}>-</button>
+    setMovies(data.Search);
+  };
+
+  return (
+    <div className="app">
+      <h1>Ice Movies</h1>
+
+      <div className="search">
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search Isaac Server for movies"
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
