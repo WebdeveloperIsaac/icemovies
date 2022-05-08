d0a5977191a39884e71db85c3a3053d0b98035ff
webdeveloperisaac <isaacinfrastructure@gmail.com>
2022-05-08 23:56:37 +0530

    Static Deployment info until Dyanamic Deployment

---
+++ src/components/Alert.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Alert = () => {
+    return(
+        <>
+        <div className="container">
+            <alert>
+            <h2>The Movie API does not Work in this Static Deployment </h2></alert>
+            <h3>Clone and Run in the Local Environment For Running Version</h3>
+        </div>
+                </>
+    )
+}
+
+export default Alert;
+
+
